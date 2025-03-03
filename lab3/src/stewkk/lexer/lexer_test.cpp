512cf6519ffae9e2c91d49df701f49a5a18f1c70
Alexandr Starovoytov <aistarovoytov@yandex.ru>
2025-03-03 19:13:13 +0300

    NextMatch interface

--- lab3/src/stewkk/lexer/lexer_test.cpp
+++ lab3/src/stewkk/lexer/lexer_test.cpp
@@ -1,11 +1,34 @@
 #include <gmock/gmock.h>
 
+#include <string_view>
+
 using ::testing::Eq;
+
+using std::string_view_literals::operator""sv;
 
 namespace stewkk::lexer {
 
-TEST(LexerTest, Simple) {
+// TODO: интерфейс на итераторах
 
+struct Token {};
+
+struct Match {
+  Token token;
+  std::string_view rest;
+};
+
+class Lexer {
+  public:
+    Match NextMatch(std::string_view text);
+};
+
+Match Lexer::NextMatch(std::string_view text) {}
+
+TEST(LexerTest, MatchesOpeningTag) {
+  Lexer l;
+  auto text = "<div>"sv;
+
+  auto [token, rest] = l.NextMatch(text);
 }
 
 }  // namespace stewkk::lexer
