eaee50dcf27b5d6dc4e99ebe5b35a7e701728e2d
Alexandr Starovoytov <aistarovoytov@yandex.ru>
2025-03-04 15:36:48 +0300

    Impl iterator

--- lab3/src/stewkk/lexer/lexer_test.cpp
+++ lab3/src/stewkk/lexer/lexer_test.cpp
@@ -4,6 +4,8 @@
 #include <optional>
 #include <string_view>
 #include <utility>
+#include <iterator>
+#include <tuple>
 
 #include <re2/re2.h>
 #include <range/v3/algorithm/max.hpp>
@@ -116,11 +118,86 @@ std::optional<Match> Matcher::NextMatch(std::string_view text, Position pos) {
     };
 }
 
+class LexerError : public std::exception {
+  public:
+    explicit LexerError(Position pos) : pos_(std::move(pos)) {}
+    Position GetPosition() const {
+      return pos_;
+    }
+  private:
+    Position pos_;
+};
+
+class Lexer {
+  public:
+    explicit Lexer(std::string text);
+
+    struct Iterator {
+      using difference_type = std::ptrdiff_t;
+      using value_type = Token;
+
+      Iterator() : token_(), rest_() {}
+      Iterator(std::string_view rest, Matcher m) : token_(), rest_(std::move(rest)), m_(std::move(m)), pos_(Position{1, 1}) {
+        auto match = m_.NextMatch(rest_, pos_);
+        if (!match.has_value()) {
+          token_ = Token{};
+          rest_ = std::string_view{};
+          return;
+        }
+        token_ = match.value().match;
+        rest_ = match.value().rest;
+        pos_.column += token_.text.size();
+      }
+
+      Token operator*() const {
+        return token_;
+      }
+
+      Iterator& operator++() {
+        auto match = m_.NextMatch(rest_, pos_);
+        if (!match.has_value()) {
+          token_ = Token{};
+          rest_ = std::string_view{};
+          return *this;
+        }
+        token_ = match.value().match;
+        rest_ = match.value().rest;
+        pos_.column += token_.text.size();
+        return *this;
+      }
+
+      Iterator operator++(int) {
+        auto tmp = *this;
+        ++*this;
+        return tmp;
+      }
+
+      bool operator==(const Iterator& other) const {
+        return token_ == other.token_ && rest_ == other.rest_;
+      }
+
+      Token token_;
+      Position pos_;
+      std::string_view rest_;
+      Matcher m_;
+    };
+    static_assert(std::forward_iterator<Iterator>);
+
+    Iterator begin() { return Iterator(text_, m_); }
+    Iterator end() { return Iterator(); }
+
+  private:
+    std::string text_;
+    Matcher m_;
+};
+
+Lexer::Lexer(std::string text) : text_(text), m_() {}
+
 TEST(MatcherTest, MatchesOpeningTag) {
   auto text = "<div> </div>"sv;
-  Matcher l;
+  Matcher m;
 
-  auto match = l.NextMatch(text, Position{1, 1});
+  auto match = m.NextMatch(text, Position{1, 1});
 
   ASSERT_THAT(match, Optional(Match{
       Token{
@@ -136,9 +213,9 @@ TEST(MatcherTest, MatchesOpeningTag) {
 
 TEST(MatcherTest, MatchesClosingTag) {
   auto text = "</div>"sv;
-  Matcher l;
+  Matcher m;
 
-  auto match = l.NextMatch(text, Position{1, 1});
+  auto match = m.NextMatch(text, Position{1, 1});
 
   ASSERT_THAT(match, Optional(Match{
       Token{
@@ -154,9 +231,9 @@ TEST(MatcherTest, MatchesClosingTag) {
 
 TEST(MatcherTest, MatchesSpaces) {
   auto text = "  \n <div>"sv;
-  Matcher l;
+  Matcher m;
 
-  auto match = l.NextMatch(text, Position{1, 1});
+  auto match = m.NextMatch(text, Position{1, 1});
 
   ASSERT_THAT(match, Optional(Match{
       Token{
@@ -172,9 +249,9 @@ TEST(MatcherTest, MatchesSpaces) {
 
 TEST(MatcherTest, MatchesLt) {
   auto text = "&lt;  \n <div>"sv;
-  Matcher l;
+  Matcher m;
 
-  auto match = l.NextMatch(text, Position{1, 1});
+  auto match = m.NextMatch(text, Position{1, 1});
 
   ASSERT_THAT(match, Optional(Match{
       Token{
@@ -190,9 +267,9 @@ TEST(MatcherTest, MatchesLt) {
 
 TEST(MatcherTest, MatchesSymbol) {
   auto text = "lt;  \n <div>"sv;
-  Matcher l;
+  Matcher m;
 
-  auto match = l.NextMatch(text, Position{1, 1});
+  auto match = m.NextMatch(text, Position{1, 1});
 
   ASSERT_THAT(match, Optional(Match{
       Token{
@@ -204,6 +281,37 @@ TEST(MatcherTest, MatchesSymbol) {
         .text = "l"s,
       },
       "t;  \n <div>"sv}));
+}
+
+TEST(LexerTest, IteratesOverTokens) {
+  Lexer l("<div> ");
+  auto it = l.begin();
+
+  ASSERT_THAT(*it, Eq(
+      Token{
+        .domain = DomainType::kOpeningTag,
+        .position = Position{
+          .line = 1,
+          .column = 1,
+        },
+        .text = "<div>"s,
+      }));
+
+  it++;
+
+  ASSERT_THAT(*it, Eq(
+      Token{
+        .domain = DomainType::kWhitespace,
+        .position = Position{
+          .line = 1,
+          .column = 6,
+        },
+        .text = " "s,
+      }));
+
+  it++;
+
+  ASSERT_THAT(it, Eq(l.end()));
 }
 
 }  // namespace stewkk::lexer
