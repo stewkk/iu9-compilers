b65be34a18d7334462030b707ce4c7f4f9427c65
Alexandr Starovoytov <aistarovoytov@yandex.ru>
2025-03-08 00:06:16 +0300

    Add immer dependency

--- lab4/src/stewkk/lexer/lexer_test.cpp
+++ lab4/src/stewkk/lexer/lexer_test.cpp
@@ -1,9 +1,28 @@
 #include <gmock/gmock.h>
 
+#include <string_view>
+
+#include <immer/vector.hpp>
+
 using ::testing::Eq;
+
+using std::string_view_literals::operator""sv;
 
 namespace stewkk::lexer {
 
-TEST(ExampleTest, Example) { ASSERT_THAT(2 + 2, Eq(4)); }
+struct Token {
+    bool operator==(const Token&) const = default;
+};
+
+struct Message {
+    bool operator==(const Message&) const = default;
+};
+
+std::pair<immer::vector<Token>, immer::vector<Message>> Tokenize(std::string_view text) {
+    return std::make_pair(immer::vector<Token>{}, immer::vector<Message>{});
+}
+
+TEST(LexerTest, Interface) {
+    ASSERT_THAT(Tokenize(""sv), Eq(std::make_pair(immer::vector<Token>{}, immer::vector<Message>{}))); }
 
 }  // namespace stewkk::lexer
